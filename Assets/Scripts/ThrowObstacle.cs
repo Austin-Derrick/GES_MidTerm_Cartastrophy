b2babac7228cad646ec29371fd56907b3d7ec9e8
austin-derrick <austinderrick15@gmail.com>
2020-03-02 15:19:01 -0600

    Skeleton code for adding a force to the thrown object

--- Assets/Scripts/ThrowObstacle.cs
+++ Assets/Scripts/ThrowObstacle.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class ThrowObstacle : MonoBehaviour
 {
-    [SerializeField] private GameObject obstaclePrefab;
+    //[SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private GameObject[] obstacleArray;
     private Rigidbody obstacleRb;
     private float throwForce = 100;
+    // Set a reference to the current prefab then apply force
+    private GameObject currentPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        obstacleRb = obstaclePrefab.GetComponent<Rigidbody>();
+        obstacleRb = obstacleArray[getRandomNumber()].GetComponent<Rigidbody>();
         InvokeRepeating("InstatiateObstacle", 0.5f, 3);
     }
 
     private void InstatiateObstacle()
     {
-        Quaternion spawnRotation = obstaclePrefab.transform.rotation;
+        int obstacleIndex = getRandomNumber();
+        Quaternion spawnRotation = obstacleArray[obstacleIndex].transform.rotation;
         Vector3 spawnPos = transform.position;
         spawnPos.y += 5.0f;
         spawnRotation.y += 90;
-        Instantiate(obstaclePrefab, spawnPos, spawnRotation);
+        Instantiate(obstacleArray[obstacleIndex], spawnPos, spawnRotation);
         throwObject();
     }
     private void throwObject()
     {
         obstacleRb.AddForce(Vector3.back * throwForce * Time.deltaTime, ForceMode.Impulse);
         obstacleRb.AddTorque(180 * 50 * Time.deltaTime, 0, 0, ForceMode.Impulse);
+    }
+
+    private int getRandomNumber()
+    {
+        return Random.Range(0, obstacleArray.Length);
     }
 }
