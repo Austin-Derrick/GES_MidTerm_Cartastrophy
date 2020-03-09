fee666c3f5e6b68b54194a73e575d2cc86ebe389
austin-derrick <austinderrick15@gmail.com>
2020-03-09 15:13:04 -0500

    Formatted bullet detection script

--- Assets/Scripts/BulletDetection.cs
+++ Assets/Scripts/BulletDetection.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BulletDetection : MonoBehaviour
 {
+    EnemyLivesManager enemyLivesManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyLivesManager = GetComponent<EnemyLivesManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            enemyLivesManager.decrementLives();
+        }
     }
 }
