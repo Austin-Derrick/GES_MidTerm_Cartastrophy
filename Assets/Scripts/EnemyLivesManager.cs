6cd055f1228c72e90a6e763f8ecd7f002cfaf1b0
austin-derrick <austinderrick15@gmail.com>
2020-03-09 15:11:35 -0500

    Added EnemyLivesManager Script to keep track of enemy health

---
+++ Assets/Scripts/EnemyLivesManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLivesManager : MonoBehaviour
+{
+    public int enemyLives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void decrementLives()
+    {
+        enemyLives--;
+    }
+    private void checkEnemyLives()
+    {
+        if (enemyLives <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
